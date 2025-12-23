a04da7bcaff327874a9c4336044d815d3374b29e
User <user@r-hlushchenko-gmt.local>
2025-12-23 21:42:56 +0200

    Added new test and refactored the project

--- src/test/java/ProjectPageTests.java
+++ src/test/java/ProjectPageTests.java
@@ -1,22 +1,88 @@
 
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import io.testomat.BaseTest;
+import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static io.testomat.utils.StringParsers.parseIntegerFromString;
 
-public class ProjectPageTests {
+public class ProjectPageTests extends BaseTest {
+
+    String url = env.get("BASE_URL");
+    String username = env.get("USERNAME");
+    String password = env.get("PASSWORD");
+    String targetProjectName = "Manufacture light";
+
+    @BeforeEach
+    void openTestomatAndLogin(){
+        open(url);
+        loginUser(username, password);
+    }
+    @AfterEach
+    void closeBrowser(){
+        Selenide.closeWebDriver();
+    }
 
     @Test
     public void findProjectTest(){
-        open("https://app.testomat.io/");
 
-        $("#content-desktop #user_email").setValue("ruslana.glushchenko@gmail.com");
-        $("#content-desktop #user_password").setValue("Uc92SE@LVPsfazC");
+        searchForProject(targetProjectName);
+        selectProject(targetProjectName);
+        waitForProjectPageIsLoaded(targetProjectName);
+
+    }
+
+    @Test
+    public void findTestsForProjectTest(){
+
+        searchForProject(targetProjectName);
+        projectTestsLabelByTitle(targetProjectName)
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("0 tests"));
+
+        SelenideElement targetProject = firstVisibleProjectCard();
+
+        Integer actualAmountOfTests = testsCountFrom(targetProject);
+        Assertions.assertEquals(0, actualAmountOfTests);
+
+    }
+
+
+
+    private SelenideElement projectTestsLabelByTitle(String projectName) {
+        return $(String.format("a[title='%s'] p", projectName));
+    }
+    private SelenideElement firstVisibleProjectCard() {
+        return $$("#grid ul li")
+                .filter(Condition.visible)
+                .shouldBe(CollectionCondition.sizeGreaterThan(0))
+                .first();
+    }
+
+    private Integer testsCountFrom(SelenideElement projectCard) {
+        return parseIntegerFromString(projectCard.$("p").getText());
+    }
+
+    private static void loginUser(String email, String password) {
+        $("#content-desktop #user_email").setValue(email);
+        $("#content-desktop #user_password").setValue(password);
         $("#content-desktop #user_remember_me").click();
         $("#content-desktop input[name='commit']").click();
-        $("#container #search").setValue("Manufacture light");
-        $("a[title='Manufacture light'] h3").click();
+    }
+    private void searchForProject(String targetProjectName){
+        $("#container #search").setValue(targetProjectName);
 
-        $("a[title='Manufacture light']").shouldBe(visible);
+    }
+    private void selectProject(String targetProjectName){
+        $(String.format("a[title='%s'] h3", targetProjectName)).click();
+    }
+    private void waitForProjectPageIsLoaded(String targetProjectName){
+        $$("a")
+                .findBy(text(targetProjectName.trim()))
+                .shouldBe(Condition.visible);
     }
 }
